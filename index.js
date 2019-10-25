6dfe716b515881e59c854b9e0b625929ce4d1f6b
P Panthong <panthari.panthong@gmail.com>
2019-10-25 10:49:45 +0200

    Setup stream router

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const streamRouter = require('./stream/router')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.listen(port, () => console.log("Server running on port ", port))
+
+app.get('/', (request, response) => {
+  console.log('get an request on /')
+  response.status(200)
+  response.send('hello world')
+})
+
+app.use(streamRouter)
---
+++ stream/router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+const router = new Router()
+
+
+router.get('/stream', (request, response) => {
+  console.log("got a request on /stream")
+  response.status(200)
+  response.send("it work")
+
+})
+
+
+module.exports = router
